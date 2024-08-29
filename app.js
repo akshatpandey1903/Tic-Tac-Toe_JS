85cdb8f5e87e56ad7b452f6b643d7786ef024b78
Akshat Pandey <akshatp1903@gmail.com>
2024-08-29 23:34:16 +0530

    Created basic working Tic-Tac-Toe game

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let boxes = document.querySelectorAll(".box");
-
 let resetButton = document.querySelector("#reset-button");
+let newGameButton = document.querySelector("#new-button");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true; //starting with player O
 
@@ -11,9 +13,37 @@ const winPatterns = [
     [1, 4, 7],
     [2, 5, 8],
     [2, 4, 6],
-    [3, 4, 5],
+    [3, 4, 5], 
     [6, 7, 8]
 ];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.style.visibility = "hidden"; 
+};
+
+//enable all boxes if we reset or start a new game
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+        box.style.backgroundColor = "beige";
+    }
+};
+
+//to disable all boxes after a player has won
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Player ${winner} is the winner`;
+    msgContainer.style.visibility = "visible";
+    disableBoxes();
+};
 
 //looping through each winning pattern and finding out if a player has won
 const checkWinner = () => {
@@ -24,7 +54,7 @@ const checkWinner = () => {
 
         if(val1 != "" && val2 != "" && val3 != ""){
             if(val1 === val2 && val2 === val3){
-                console.log(`Player ${val1} has won the game`);
+                showWinner(val1);
             }
         }
     }
@@ -32,7 +62,6 @@ const checkWinner = () => {
 
 boxes.forEach((box) => {
     box.addEventListener("click", (e) => {
-        console.log("Box was clicked");
         if(turnO === true){ //player O's turn
             box.innerText = "O";
             turnO = false;
@@ -46,3 +75,6 @@ boxes.forEach((box) => {
         checkWinner();  
     });
 });
+
+newGameButton.addEventListener("click", resetGame);
+resetButton.addEventListener("click", resetGame);
